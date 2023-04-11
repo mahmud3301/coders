2828369b3806ca08207ca678745b82f4dac39455
mahmud3301 <mahmudulhasan13189@gmail.com>
2023-04-11 23:25:35 +0600

    LocalStorage done

--- src/assets/Components/JobDetails/JobDetails.jsx
+++ src/assets/Components/JobDetails/JobDetails.jsx
@@ -7,6 +7,7 @@ import salaryImg from '../../Icons/Frame.png'
 import job from '../../Icons/Frame-1.png'
 import phoneImg from '../../Icons/Frame-2.png'
 import emailImg from '../../Icons/Frame-3.png'
+import toast, { Toaster } from 'react-hot-toast';
 
 const JobDetails = () => {
     const jobDetails = useParams();
@@ -21,7 +22,31 @@ const JobDetails = () => {
             setDatas(values);
         }
     }, []);
-    const { job_description, job_responsibility, educational_requirements, experiences, salary, job_title, location, email, phone } = datas;
+    
+    const handleAppNow = (id) => {
+        let data = [];
+        const storedData = localStorage.getItem("data");
+        if (storedData) {
+            data = JSON.parse(storedData);
+        }
+        if (data) {
+            if (!data.includes(id)) {
+                data.push(id);
+                localStorage.setItem("data", JSON.stringify(data));
+                toast.success("Job Applied Successfully");
+            } else {
+                toast.error("Job Already Applied");
+            }
+        } else {
+            data = [id];
+            localStorage.setItem("data", JSON.stringify(data));
+            toast.success("Job Applied Successfully");
+        }
+    }
+
+
+
+    const { job_description, job_responsibility, educational_requirements, experiences, salary, job_title, location, email, phone, id } = datas;
 
     return (
         <div>
@@ -35,15 +60,15 @@ const JobDetails = () => {
                 <div>
                     <img src={vector} alt="" />
                 </div>
-            </div>
+            </div><br /><br /><br /><br />
             <div className="p-7 lg:flex">
-                <div className="w-[67%]">
+                <div className="lg:w-[67%] w-full">
                     <p className="text-lg"><span className="text-xl font-bold bg-gradient-to-r text-transparent bg-clip-text from-indigo-500 to-purple-600">Job Description:</span> {job_description}</p><br /><br />
                     <p className="text-lg"><span className="text-xl font-bold bg-gradient-to-r text-transparent bg-clip-text from-indigo-500 to-purple-600">Job Responsibility:</span> {job_responsibility}</p><br /><br />
                     <p className="text-lg"><span className="text-xl font-bold bg-gradient-to-r text-transparent bg-clip-text from-indigo-500 to-purple-600">Educational Requirements:</span> {educational_requirements}</p><br /><br />
                     <p><span className="text-xl font-bold bg-gradient-to-r text-transparent bg-clip-text from-indigo-500 to-purple-600">Experience:</span>  {experiences}</p>
                 </div>
-                <div className="w-[33%] ">
+                <div className="lg:w-[33%] w-full">
                     <div className="border-2 border-purple-400 mt-7 lg:mt-0 p-4 bg-black">
                         <p className="text-xl font-bold bg-gradient-to-r text-transparent bg-clip-text from-indigo-500 to-purple-600">Job Details</p><br />
                         <hr className="border-1 border-purple-400" /><br />
@@ -74,10 +99,16 @@ const JobDetails = () => {
                         </div>
                     </div><br />
                     <div>
-                        <button className="p-5 rounded-lg bg-gradient-to-r text bg-clip-btn from-indigo-500 to-purple-600 text-white text-center w-full">Apply Now</button>
+                        <button
+                            className="p-5 rounded-lg bg-gradient-to-r text bg-clip-btn from-indigo-500 to-purple-600 text-white text-center w-full"
+                            onClick={() => handleAppNow(id)}
+                        >
+                            Apply Now
+                        </button>
                     </div>
                 </div>
             </div>
+            <Toaster/>
         </div>
     );
 };
